05eb3e7d7ca65176a471b007b952e7810cf83d10
Sean Morris <sean@seanmorr.is>
2020-09-12 10:58:10 -0400

    Styling tweak for network prompts.

--- public/app.js
+++ public/app.js
@@ -724,7 +724,7 @@ var Bindable = /*#__PURE__*/function () {
               target.___before___[_i7](target, key, target[Stack], object, providedArgs);
             }
 
-            var objRef = object instanceof Promise || object instanceof EventTarget || object instanceof MutationObserver || object instanceof IntersectionObserver || object instanceof MutationObserver || object instanceof PerformanceObserver || object instanceof ResizeObserver || object instanceof Map || object instanceof Set ? object : object[Ref];
+            var objRef = object instanceof Promise || object instanceof EventTarget || object instanceof MutationObserver || object instanceof IntersectionObserver || object instanceof MutationObserver || object instanceof PerformanceObserver || typeof ResizeObserver === 'function' && object instanceof ResizeObserver || object instanceof Map || object instanceof Set ? object : object[Ref];
             var ret = new.target ? _construct(target[key], providedArgs) : target[key].apply(objRef || object, providedArgs);
 
             for (var _i8 in target.___after___) {
@@ -2782,7 +2782,11 @@ var View = /*#__PURE__*/function () {
           expandProperty = _Bindable$resolve2[1];
 
       tag.removeAttribute('cv-expand');
-      console.log(proxy, expandProperty);
+
+      if (!proxy[expandProperty]) {
+        proxy[expandProperty] = {};
+      }
+
       this.onRemove(tag[expandBind] = proxy[expandProperty].bindTo(function (v, k, t, d, p) {
         if (d || v === undefined) {
           tag.removeAttribute(k, v);
@@ -5077,9 +5081,7 @@ exports.Console = void 0;
 
 var _View2 = require("curvature/base/View");
 
-var _Socket = require("subspace-client/Socket");
-
-var _MeltingText = require("view/MeltingText");
+var _MeltingText = require("./view/MeltingText");
 
 var _Task = require("./Task");
 
@@ -5126,16 +5128,12 @@ var Console = /*#__PURE__*/function (_View) {
 
     _classCallCheck(this, Console);
 
-    console.log(options.path);
-    console.log(options);
     _this = _super.call(this, args);
     var defaults = {
       init: false,
       path: _Path.Path
     };
     var allOptions = Object.assign({}, defaults, options);
-    console.log(allOptions.path);
-    console.log(allOptions);
     _this.template = "<div class = \"terminal [[inverted]]\" cv-on = \"click:focus(event)\">\n\t<div class = \"output\" cv-each = \"output:line:l\" cv-ref = \"output:curvature/base/Tag\">\n\t\t<p>[[line]]</p>\n\t</div>\n\t<div class = \"bottom\">\n\t\t<div>[[prompt]]&nbsp;</div>\n\t\t<div>\n\t\t\t<form cv-on = \"submit:cancel(event)\">\n\t\t\t\t<textarea\n\t\t\t\t\tcv-bind = \"input\"\n\t\t\t\t\tcv-on   = \":keydown(event);:keyup(event)\"\n\t\t\t\t\tcv-ref  = \"input:curvature/base/Tag\"\n\t\t\t\t\trow     = \"1\"\n\t\t\t\t></textarea>\n\t\t\t</form>\n\n\t\t\t<form cv-on = \"submit:cancel(event)\">\n\t\t\t\t<input\n\t\t\t\t\tautocomplete = \"one-time-code\"\n\t\t\t\t\tname    = \"pw-input\"\n\t\t\t\t\ttype    = \"password\"\n\t\t\t\t\tcv-bind = \"input\"\n\t\t\t\t\tcv-ref  = \"password:curvature/base/Tag\"\n\t\t\t\t\tcv-on   = \":keydown(event,false);:keyup(event,false)\"\n\t\t\t\t/>\n\t\t\t</form>\n\n\t\t\t<input\n\t\t\t\tcv-on  = \"input:fileLoaded(event)\"\n\t\t\t\tcv-ref = \"file:curvature/base/Tag\"\n\t\t\t\tname   = \"file-input\"\n\t\t\t\ttype   = \"file\"\n\t\t\t\tstyle  = \"display: none\"\n\t\t\t/>\n\t\t</div>\n\t</div>\n</div>\n\n<div class = \"scanlines\"></div>\n";
     _this.args.input = '';
     _this.args.output = [];
@@ -5164,11 +5162,15 @@ var Console = /*#__PURE__*/function (_View) {
         }
       }
 
+      var scroller = _this.scroller;
+      var scrollTo = scroller === window ? document.body.scrollHeight : scroller.scrollHeight;
+      console.log(scroller, scroller.scrollHeight);
+
       _this.onNextFrame(function () {
-        window.scrollTo({
-          top: document.body.scrollHeight,
+        scroller.scrollTo({
+          behavior: 'smooth',
           left: 0,
-          behavior: 'smooth'
+          top: scroller.scrollHeight
         });
       });
     });
@@ -5177,6 +5179,7 @@ var Console = /*#__PURE__*/function (_View) {
       _this.runScript(allOptions.init);
     }
 
+    _this.scroller = allOptions.scroller || window;
     _this.path = allOptions.path || {};
     _this.originalInput = '';
     return _this;
@@ -5326,7 +5329,7 @@ var Console = /*#__PURE__*/function (_View) {
 
           var _command = args.shift().trim();
 
-          if (_command.substr(-1) == "?") {
+          if (_command.length > 1 && _command.substr(-1) == "?") {
             _command = _command.substr(0, _command.length - 1);
 
             if (_command in this.path) {
@@ -5380,14 +5383,13 @@ var Console = /*#__PURE__*/function (_View) {
         this.tasks.unshift(task);
 
         var output = function output(event) {
-          var prompt = task.prompt || _this4.args.prompt || '::';
+          var prompt = task.outPrompt || task.prompt || _this4.args.prompt || '::';
 
           _this4.args.output.push("".concat(prompt, " ").concat(event.detail));
         };
 
         var error = function error(event) {
           var errorPrompt = task.errorPrompt || '!!';
-          _this4.args.prompt = errorPrompt;
 
           _this4.args.output.push("".concat(errorPrompt, " ").concat(event.detail));
         };
@@ -5401,6 +5403,7 @@ var Console = /*#__PURE__*/function (_View) {
         task["catch"](function (error) {
           return _this4.args.output.push("!! ".concat(error));
         });
+        this.args.prompt = task.prompt;
         task["finally"](function (done) {
           task.removeEventListener('error', error);
           task.removeEventListener('output', output);
@@ -5892,6 +5895,280 @@ exports.TaskSignals = TaskSignals;
 _defineProperty(TaskSignals, "KILL", 'kill');
 
 _defineProperty(TaskSignals, "CLOSE", 'close');
+  })();
+});
+
+require.register("subspace-console/view/MeltingText.js", function(exports, require, module) {
+  require = __makeRelativeRequire(require, {}, "subspace-console");
+  (function() {
+    "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.MeltingText = void 0;
+
+var _View = require("curvature/base/View");
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var MeltingText = /*#__PURE__*/function (_BaseView) {
+  _inherits(MeltingText, _BaseView);
+
+  var _super = _createSuper(MeltingText);
+
+  function MeltingText(args) {
+    var _this;
+
+    _classCallCheck(this, MeltingText);
+
+    _this = _super.call(this, args);
+    _this.last = _this.init = Date.now();
+    _this.charUp = [// '\u030d', /*     ̍     */		'\u030e', /*     ̎     */		'\u0304', /*     ̄     */		'\u0305', /*     ̅     */
+    // '\u033f', /*     ̿     */		'\u0311', /*     ̑     */		'\u0306', /*     ̆     */		'\u0310', /*     ̐     */
+    // '\u0352', /*     ͒     */		'\u0357', /*     ͗     */		'\u0351', /*     ͑     */		'\u0307', /*     ̇     */
+    // '\u0308', /*     ̈     */		'\u030a', /*     ̊     */		'\u0342', /*     ͂     */		'\u0343', /*     ̓     */
+    "\u0344",
+    /*     ̈́     */
+
+    /*	'\u034a', /*     ͊     */
+
+    /*	'\u034b', /*     ͋     */
+
+    /*	'\u034c', /*     ͌     */
+    "\u0303",
+    /*     ̃     */
+
+    /*	'\u0302', /*     ̂     */
+
+    /*	'\u030c', /*     ̌     */
+
+    /*	'\u0350', /*     ͐     */
+    "\u0300"
+    /*     ̀     */
+    //	'\u0301', /*     ́     */		'\u030b', /*     ̋     */		'\u030f', /*     ̏     */
+    // '\u0312', /*     ̒     */		'\u0313', /*     ̓     */		'\u0314', /*     ̔     */		'\u033d', /*     ̽     */
+    // '\u0309', /*     ̉     */		'\u0363', /*     ͣ     */		'\u0364', /*     ͤ     */		'\u0365', /*     ͥ     */
+    // '\u0366', /*     ͦ     */		'\u0367', /*     ͧ     */		'\u0368', /*     ͨ     */		'\u0369', /*     ͩ     */
+    // '\u036a', /*     ͪ     */		'\u036b', /*     ͫ     */		'\u036c', /*     ͬ     */		'\u036d', /*     ͭ     */
+    // '\u036e', /*     ͮ     */		'\u036f', /*     ͯ     */		'\u033e', /*     ̾     */		'\u035b', /*     ͛     */
+    ];
+    _this.charMid = ["\u0315",
+    /*     ̕     */
+    "\u031B",
+    /*     ̛     */
+    "\u0340",
+    /*     ̀     */
+    "\u0341",
+    /*     ́     */
+    "\u0358",
+    /*     ͘     */
+    "\u0321",
+    /*     ̡     */
+    "\u0322",
+    /*     ̢     */
+    "\u0327",
+    /*     ̧     */
+    "\u0328",
+    /*     ̨     */
+    "\u0334",
+    /*     ̴     */
+    "\u0335",
+    /*     ̵     */
+    "\u0336",
+    /*     ̶     */
+    "\u034F",
+    /*     ͏     */
+    "\u035C",
+    /*     ͜     */
+    "\u035D",
+    /*     ͝     */
+    "\u035E",
+    /*     ͞     */
+    "\u035F",
+    /*     ͟     */
+    "\u0360",
+    /*     ͠     */
+
+    /*'\u0362',      ͢     */
+    "\u0338",
+    /*     ̸     */
+    "\u0337",
+    /*     ̷     */
+    "\u0361"
+    /*     ͡     */
+
+    /*'\u0489'     ҉_     */
+    ];
+    _this.charDown = [// '\u0316', /*     ̖     */		'\u0317', /*     ̗     */		'\u0318', /*     ̘     */		'\u0319', /*     ̙     */
+    // '\u0316', /*     ̖     */		'\u0317', /*     ̗     */		'\u0318', /*     ̘     */		'\u0319', /*     ̙     */
+    // '\u0320', /*     ̠     */		'\u0324', /*     ̤     */		'\u0325', /*     ̥     */		'\u0326', /*     ̦     */
+    // '\u0329', /*     ̩     */		'\u032a', /*     ̪     */		'\u032b', /*     ̫     */		'\u032c', /*     ̬     */
+    // '\u032d', /*     ̭     */		'\u032e', /*     ̮     */		'\u032f', /*     ̯     */		'\u0330', /*     ̰     */
+    // '\u0331', /*     ̱     */		'\u0332', /*     ̲     */		'\u0333', /*     ̳     */		'\u0339', /*     ̹     */
+    "\u033A",
+    /*     ̺     */
+    "\u033B",
+    /*     ̻     */
+    "\u033C",
+    /*     ̼     */
+    "\u0345"
+    /*     ͅ     */
+    //'\u0347', /*     ͇     */		'\u0348', /*     ͈     */		'\u0349', /*     ͉     */		'\u034d', /*     ͍     */
+    //'\u034e', /*     ͎     */		'\u0353', /*     ͓     */		'\u0354', /*     ͔     */		'\u0355', /*     ͕     */
+    // '\u0356', /*     ͖     */		'\u0359', /*     ͙     */		'\u035a', /*     ͚     */		'\u0323' /*     ̣     */
+    ];
+    _this.template = "\n\t\t\t<div cv-bind = \"output\" class = \"melting\"></div>\n\t\t";
+    _this.args.input = "Magic is no more than the art of employing consciously invisible means to produce visible effects. Will, love, and imagination are magic powers that everyone possesses; and whoever knows how to develop them to their fullest extent is a magician. Magic has but one dogma, namely, that the seen is the measure of the unseen\n"; // this.args.input      = 'anything';
+
+    _this.args.output = 'uh.';
+    _this.corruptors = [];
+    _this.maxMaxCorrupt = 25;
+    _this.maxCorrupt = 0;
+    _this.type = '';
+
+    _this.onFrame(function () {
+      _this.typewriter(_this.args.input);
+    });
+
+    _this.onInterval(16 * 4, function () {
+      var selection = window.getSelection();
+
+      if (selection.anchorOffset !== selection.focusOffset) {
+        return;
+      }
+
+      if (selection.anchorNode !== selection.focusNode) {
+        return;
+      }
+
+      _this.args.output = _this.corrupt(_this.type); // this.args.output = this.type;
+    });
+
+    _this.args.bindTo('input', function (v) {
+      _this.type = '';
+      _this.corruptors = [];
+    });
+
+    return _this;
+  }
+
+  _createClass(MeltingText, [{
+    key: "age",
+    value: function age() {
+      return this.init - Date.now();
+    }
+  }, {
+    key: "lastFrame",
+    value: function lastFrame() {
+      return this.last - Date.now();
+    }
+  }, {
+    key: "corrupt",
+    value: function corrupt(v) {
+      if (v.length * 1.15 < this.args.input.length) {
+        return this.type;
+      }
+
+      var chars = v.split('');
+
+      var random = function random(x) {
+        return parseInt(Math.random() * x);
+      };
+
+      if (random(1024) < 256 && this.maxCorrupt < this.maxMaxCorrupt) {
+        this.maxCorrupt += 5;
+      }
+
+      for (var _i in chars) {
+        this.corruptors[_i] = this.corruptors[_i] || [];
+
+        if (chars[_i].match(/\W/)) {
+          continue;
+        }
+
+        var charSets = [// this.charDown // Melt Slow
+        this.charDown, this.charMid // Melt
+        // this.charDown, this.charUp,   this.charMid, // Boil
+        // this.charMid, this.charUp, // Burn
+        // this.charMid // Simmer
+        // this.charUp // Rain
+        ];
+        var charSet = charSets[random(charSets.length)];
+
+        if (random(8192) < 1) {
+          this.corruptors[_i].unshift(charSet[random(charSet.length)]);
+        }
+
+        if (this.corruptors[_i].length < this.maxCorrupt) {
+          this.corruptors[_i].unshift(charSet[random(charSet.length)]);
+        }
+
+        if (random(2048) < 1 && this.maxCorrupt > 25) {
+          this.corruptors[_i].splice(5 * random(5));
+        }
+
+        this.corruptors[_i].push(this.corruptors[_i].shift());
+      }
+
+      for (var i in chars) {
+        if (this.corruptors[i]) {
+          chars[i] += this.corruptors[i].join('');
+        }
+      }
+
+      return chars.join('');
+    }
+  }, {
+    key: "typewriter",
+    value: function typewriter(v) {
+      this.type = this.type || '';
+
+      if (this.type !== v) {
+        this.type += v.substr(this.type.length, 1);
+        this.onTimeout(150, function () {
+          var max = window.scrollY + window.innerHeight;
+
+          if (document.body.scrollHeight > max) {
+            window.scrollTo({
+              top: document.body.scrollHeight,
+              left: 0,
+              behavior: 'smooth'
+            });
+          }
+        });
+      } else {
+        return true;
+      }
+
+      return false;
+    }
+  }]);
+
+  return MeltingText;
+}(_View.View);
+
+exports.MeltingText = MeltingText;
   })();
 });
 require.register("Config.js", function(exports, require, module) {
